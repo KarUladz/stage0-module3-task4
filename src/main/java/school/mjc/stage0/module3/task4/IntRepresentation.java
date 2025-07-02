aa8a92e4c60073ce99b376da5a9040f609af31e3
KarUladz <u.karnialiuk@gmail.com>
2025-07-02 15:20:11 +0300

    feat: add m3-task4

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int[] chars = {99, 104, 97, 114};
+        for (int ch : chars) {
+            System.out.print((char) ch);
+        }
     }
 }
